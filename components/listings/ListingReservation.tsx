e7839c6c5b9a98b9f39a4973d2312ceeb901d75c
ARAVIND <aravindgshanthi@gmail.com>
2023-08-28 10:21:24 +0530

    Fix spelling mistack

--- components/listings/ListingReservation.tsx
+++ components/listings/ListingReservation.tsx
@@ -45,7 +45,7 @@ const ListingReservation: React.FC<ListingReservationProps> = ({
       />
       <hr />
       <div className="p-4">
-        <Button disabled={disabled} label="Reserver" onClick={onSubmit} />
+        <Button disabled={disabled} label="Reserve" onClick={onSubmit} />
       </div>
       <div
         className="
